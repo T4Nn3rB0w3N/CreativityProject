ce34130cac4233fa958b6c035fd197f92bfad45f
Bowen <tbow1382@740s-pl-120.local>
2018-11-01 07:55:33 -0600

    Initial commit

---
+++ CreativityProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CreativityProject
+//
+//  Created by Bowen, Tanner on 11/1/18.
+//  Copyright © 2018 CSP. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
