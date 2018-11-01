dba58c5ac870d2e656855a6956200491ddbf7951
Bowen <tbow1382@740s-pl-120.local>
2018-11-01 09:04:01 -0600

    The changes I made:
    Created a View in Main.storyboard that is colored Maroon
    Created DrawingView.swift
    Made all override functions explicitly public

---
+++ CreativityProject/View/Base.lproj/DrawingView.swift
@@ -0,0 +1,30 @@
+//
+//  DrawingView.swift
+//  CreativityProject
+//
+//  Created by Bowen, Tanner on 11/1/18.
+//  Copyright © 2018 CSP. All rights reserved.
+//
+
+import UIKit
+
+public class DrawingView: UIView
+{
+
+    
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    public override func draw(_ rect: CGRect) -> Void
+    {
+        //Drawing code
+    }
+
+    private func createStickFigure() -> UIBezierPath
+    {
+        let figure : UIBezierPath = UIBezierPath()
+        
+        
+        return figure
+    }
+    
+}
