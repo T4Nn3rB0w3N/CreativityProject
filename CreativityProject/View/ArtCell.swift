905016ee8431978254b62c64dcb6b44d9f152d3e
Bowen <tbow1382@740s-pl-120.local>
2018-11-07 09:42:23 -0700

    Created a UICollectionViewCell, "ArtCell", and created an image view and a label in artIdentifier.
    There are problems with getting the imageView and label and artIdentifier to have equal sizes.

---
+++ CreativityProject/View/ArtCell.swift
@@ -0,0 +1,14 @@
+//
+//  ArtCell.swift
+//  CreativityProject
+//
+//  Created by Bowen, Tanner on 11/7/18.
+//  Copyright © 2018 CSP. All rights reserved.
+//
+
+import UIKit
+
+public class ArtCell: UICollectionViewCell
+{
+    
+}
